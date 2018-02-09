860ac4b9c0142a8b663f08bca05af214d6ecf1d5
IanMcT <ian.mctavish@tldsb.on.ca>
2018-02-09 09:01:09 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Ian McTavish
+ * Feb 8, 2018
+ * Output
+ * User types a name and it outputs Hello name!
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u1_02_McTOutput
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            lblOutput.Content = "Hello, " + entryName.Text;
+        }
+    }
+}
